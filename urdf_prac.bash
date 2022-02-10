24d8cf45e21fad8a1ed134b193c90573e4fe3255
Pupking <dsh17082001@gmail.com>
2022-02-10 22:00:07 +0530

    Added the script missing previously

---
+++ urdf_prac.bash
@@ -0,0 +1,50 @@
+cd
+
+DIR="RobotProject"
+if test -d $DIR; then
+    cd "$DIR"
+else
+    mkdir $DIR
+fi
+
+cd $DIR
+
+WS="practice_ws"
+
+if test -d $WS; then
+    cd "$WS"
+else
+    mkdir -p "$WS/src"
+fi
+
+cd $WS
+
+catkin_make
+
+cd src/
+
+
+if [ -d "myrobot" ]; then
+    echo "myrobot package exists already."
+    cd ../
+    catkin_make
+else
+    catkin_create_pkg myrobot urdf
+fi
+
+source devel/setup.bash
+
+cd myrobot
+
+if [ -d "urdf" ]; then
+    echo "urdf directory exists already."
+else
+    mkdir "urdf"
+fi
+
+if [ -d "launch" ]; then
+    echo "launch directory exists already."
+else
+    mkdir "launch"
+fi
+
